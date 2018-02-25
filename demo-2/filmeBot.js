8f106a366ab176742ce774301a9d95fe13477794
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-25 20:54:24 -0300

    Renomeando o arquivo.

--- demo-2/filmeBot.js
+++ demo-2/filmeBot.js
@@ -1,6 +1,6 @@
 /**
  * 
- * Arquivo: bot.js
+ * Arquivo: filmeBot.js
  * Data: 25/02/2018
  * Descrição: Desenvolvimento de um Bot usando a api do The Movie Db
  * Author: Glaucia Lemos
@@ -17,5 +17,5 @@ bot.dialog('/', [
     session => 
         builder.Prompts.text(session, `Olá! Tudo bem? Eu sou o FilmeBot. Qual é o seu nome?`),
     (session, results) =>
-        session.send('Olá %s', results.response),
+        session.send('Olá %s!', results.response),
 ]);
