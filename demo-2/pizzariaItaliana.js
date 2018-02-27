14b4d83a21414b1ccf07162653e2f77964d6bbff
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-26 22:56:18 -0300

    Criando um Bot de pedido de pizzas

---
+++ demo-2/pizzariaItaliana.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Arquivo: pizzariaItaliana.js
+ * Data: 26/02/2018
+ * Descrição: Desenvolvimento de um Bot de pedido de pizza integrado com o LUIS.
+ * Author: Glaucia Lemos
+ *
+ */
+
+ 
