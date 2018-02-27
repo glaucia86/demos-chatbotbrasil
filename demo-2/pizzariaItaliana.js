318707a6f275c99ed82c96e3a14a92e155c0ab44
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-02-26 23:19:28 -0300

    Desenvolvimento das intents no app.

--- demo-2/pizzariaItaliana.js
+++ demo-2/pizzariaItaliana.js
@@ -17,7 +17,7 @@ const restify = require('restify');
 
 const server = restify.createServer();
 
-//Configuração do Bot:
+//===> Configuração do Bot:
 const connector = new builder.ChatConnector({
     appId: '',
     appPassword: ''
@@ -25,7 +25,30 @@ const connector = new builder.ChatConnector({
 
 const bot = new builder.UniversalBot(connector);
 
-//Configuração LUIS:
+//===> Configuração LUIS:
 const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 const intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
+//===> Configuração dos 'Intents'(Intenções):
+
+//Endpoint - Saudar:
+intents.matches('Saudar', (session, results) => {
+    session.send('Oi! Tudo bem? Em que eu posso ajudar?');
+});
+
+//Endpoint - Pedir:
+intents.matches('Pedir', [(session, args, next) => {
+    const pizzas = ['Quatro Queijos', 'Calabreza', 'Frango Catupiri', 'Margarita', 'Portuguesa', 'Mussarela', 'Especialida'];
+    const entityPizza = builder.EntityRecognizer.findEntity(args.entities, 'Pizza');
+
+    //Aqui estaremos verificando com o LUIS os melhores 'matches' para a solicitação
+    //do pedido da pizza através da Entidade: Pizza:
+    if (entityPizza) {
+        const match = builder.EntityRecognizer.findBestMatch(pizzas, entityPizza.entity);
+    }
+    
+    if (!match) {
+
+    }
+}
+]);
